103d03721b874e2a3ee386beaa806d9b02ba5844
ietheredge <rianetheredge@gmail.com>
2020-07-18 13:15:23 +0200

    updated config and setup file

--- setup.py
+++ setup.py
@@ -9,23 +9,5 @@ setup(
     author='R. Ian Etherdge',
     author_email='ietheredge@ab.mpg.de',
     license='MIT',
-    packages=[
-        'VisionEngine',
-        'VisionEngine.datasets',
-        'VisionEngine.utils',
-        'VisionEngine.data_loaders',
-        'VisionEngine.base',
-        'VisionEngine.models',
-        'VisionEngine.trainers'
-        ],
-    zip_safe=False,
-    package_dir={
-        'VisionEngine': 'VisionEngine',
-        'VisionEngine.datasets': 'VisionEngine/data_loaders/datasets',
-        'VisionEngine.utils': 'VisionEngine/utils',
-        'VisionEngine.data_loaders': 'VisionEngine/data_loaders',
-        'VisionEngine.base': 'VisionEngine/base',
-        'VisionEngine.models': 'VisionEngine/models',
-        'VisionEngine.trainers': 'VisionEngine/trainers'
-        }
+    packages=find_packages(include=["VisionEngine*"])
 )
